a17090a8bd49eae81acb89b56de23897f0fe93ce
enochsys <enochalvarez07@gmail.com>
2026-03-30 15:27:59 +0200

    Add text_node_to_html_node converter with tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from pydoc import text
+
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -25,3 +28,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise Exception("wrong text_type")
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(
+            tag="img", value="", props={"src": text_node.url, "alt": text_node.text}
+        )
