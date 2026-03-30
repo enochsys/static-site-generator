4e7474369172550453f0c2db0fd51fa396276347
enochsys <enochalvarez07@gmail.com>
2026-03-30 14:35:28 +0200

    Add LeafNode subclass with HTML rendering support and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -49,6 +49,18 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me", props={"href": "https://boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://boot.dev">Click me</a>')
 
 
 if __name__ == "__main__":
