866485487eb441f012de1d79c1377fc124f9ead7
enochsys <enochalvarez07@gmail.com>
2026-03-31 20:02:01 +0200

    feat: add recursive page generation from content directory

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 dir_path_blog = os.path.join(dir_path_content, "blog")
 dir_path_contact = os.path.join(dir_path_content, "contact")
 template_path = "./template.html"
+basepath = sys.argv[0] if len(sys.argv) > 0 else "/"
 
 
 def main():
+
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 main()
